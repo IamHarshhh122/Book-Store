ce88448ba7489b131e177b218136d05b110d315c
Harsh bhatta <harshbhatta5@gmail.com>
2026-07-26 15:47:53 +0530

    Enhance CORS configuration and update MongoDB connection log

--- backend/index.js
+++ backend/index.js
@@ -7,18 +7,30 @@ import userRoute from "./route/user.route.js";
 import bookRoute from "./route/book.route.js"; 
 import aiBookRoute from "./route/aiBook.route.js";
 
-
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "https://book-store-frontend-93kc.onrender.com"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
+
 app.use(express.json());
 
 const PORT = process.env.PORT || 4001;
 const URI = process.env.MONGODB_URI;
 
+app.get("/", (req, res) => {
+    res.send("BookStore Backend Server is Running Smoothly!");
+});
+
 mongoose.connect(URI)
-    .then(() => console.log("Connected to MongoDB"))
+    .then(() => console.log("Connected to MongoDB Successfully"))
     .catch((error) => console.log("MongoDB Error: ", error));
 
 app.use("/user", userRoute);
