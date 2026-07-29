37b37e7b525ae67a369514d8532700c40f190942
Harsh bhatta <harshbhatta5@gmail.com>
2026-07-29 22:51:19 +0530

    Add deleteUser route to user.route.js

--- backend/route/user.route.js
+++ backend/route/user.route.js
@@ -6,9 +6,12 @@ import {
     purchaseBook, 
     getMyBooks, 
     getAllAdminsData, 
-    submitReview 
+    submitReview,
+    deleteUser
 } from "../controller/user.controller.js"; 
+
 const router = express.Router();    
+
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
@@ -18,5 +21,6 @@ router.get("/mybooks/:userId", getMyBooks);
 
 router.post("/submit-review", submitReview); 
 router.get("/admin-all-users", getAllAdminsData);
+router.delete("/delete-user/:id", deleteUser);
 
 export default router;
